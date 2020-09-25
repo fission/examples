dc8eea83634494ae36ba9592eb7399f9207ec5fb
Rahul Bhati <rjbhati009@gmail.com>
2020-09-25 13:46:16 +0530

    Added large scale example (#7)

---
+++ samples/long-running-compute/hello-2-90.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import math
+import time
+
+def main():
+    start = datetime.now()
+    i = 10
+    while (datetime.now() - start).seconds <= 90:
+        print(math.factorial(i), datetime.now())
+        time.sleep(0.5)
+        i += 10
