2b4e753e4417271c04ded9124693ffd2ef2a7207
Vishal Biyani <vrbiyani@gmail.com>
2020-10-27 14:12:19 +0530

    Test scripts etc.

--- scripts/cluster-setup/setupcluster.sh
+++ scripts/cluster-setup/setupcluster.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 echo "Starting Eksctl cluster setup"
 # eksctl create cluster -f cluster.yaml # Till --asg-access is supported in YAML file, we need to use CLI
-eksctl create cluster --name scale-test --version 1.17 --managed --asg-access --region ap-south-1 --node-type=c4.4xlarge --nodes-min 3 --nodes-max 20 --ssh-access 
+eksctl create cluster --name fission-scale --version 1.17 --managed --asg-access --region ap-south-1 --node-type=c4.4xlarge --nodes-min 1 --nodes-max 20 --ssh-access 
 
 echo "Getting nodes"
 kubectl get nodes
@@ -20,7 +20,9 @@ helm install fission-metrics --namespace monitoring prometheus-community/kube-pr
   --set prometheus.prometheusSpec.podMonitorSelectorNilUsesHelmValues=false \
   --set prometheus.prometheusSpec.ruleSelectorNilUsesHelmValues=false
 
-# helm install --namespace fission --name-template fission --set prometheus.enabled=false https://github.com/fission/fission/releases/download/1.10.0/fission-all-1.10.0.tgz
-# kubectl annotate svc/router -nfission service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout: "3600"
+helm install --namespace fission --name-template fission --set prometheus.enabled=false,nats.enabled=false,executor.podReadyTimeout=3600s,router.unTapServiceTimeout=4800s https://github.com/fission/fission/releases/download/1.11.2/fission-all-1.11.2.tgz
+
+# kubectl annotate svc/router -nfission service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout: "4800"
+
 # kubectl apply -f servicemonitors.yaml --namespace fission 
 # kubectl --namespace monitoring port-forward svc/fission-metrics-grafana 3000:80
