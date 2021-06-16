d33150340d29d46a62f7cc441e46cecd7b705ac0
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2021-06-16 19:04:25 +0530

    add web function
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

---
+++ samples/websocket/web/app.py
@@ -0,0 +1,23 @@
+import os.path
+import logging
+from flask import request
+
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(root_dir(), filename)
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+
+def main():
+    # print(request)
+    # print(request.headers)
+    return get_file(request.headers['X-Fission-Params-Html'])
