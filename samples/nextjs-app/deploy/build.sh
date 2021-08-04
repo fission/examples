29ca0a39e42a4ec032ca1ccc55c7256073dd0836
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2021-08-04 11:34:48 +0530

    nextjs app
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

---
+++ samples/nextjs-app/deploy/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+doit() {
+  echo "@@@@ $@"
+  "$@"
+}
+
+BUILDER=nodejs-nextjs-builder:latest
+ENVIRONMENT=nodejs-nextjs-env:latest
+doit docker build -t $BUILDER deploy/builder/ -f deploy/builder/Dockerfile-12.16
+doit docker build -t $ENVIRONMENT deploy/environment/ -f deploy/environment/Dockerfile-12.16
+
+doit kind load docker-image $BUILDER
+doit kind load docker-image $ENVIRONMENT
+
+doit docker push $BUILDER
+doit docker push $ENVIRONMENT
+
+doit zip -r nextjs-source.zip app.js next.config.js package.json package.lock.json data/ pages/ public/ styles/ node_modules/ .next/
+
+doit fission spec apply --specdir='./deploy/specs'
---
+++ samples/nextjs-app/deploy/specs/README.md
@@ -0,0 +1,23 @@
+# Spec commands
+
+## Spec Generation
+
+```sh
+fission spec init
+fission env create --spec --name nodejs --image nodejs-nextjs-env --builder nodejs-nextjs-builder
+fission package create --spec --src nextjs-source.zip --env nodejs --name nextjs-source
+fission fn create --spec --name nextjs-func --pkgname nextjs-source --entrypoint "app"
+fission route create --spec --method GET --method POST --prefix "/nextapp/" --function nextjs-func
+```
+
+## Spec Apply
+
+```sh
+fission spec apply
+```
+
+## Spec Destroy
+
+```sh
+fission spec destroy
+```
