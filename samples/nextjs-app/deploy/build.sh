534b652147eda52bece0868e6e7e084b246b1d45
Sanket Sudake <sanketsudake@gmail.com>
2021-08-06 10:45:13 +0530

    Use default fission env
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- samples/nextjs-app/deploy/build.sh
+++ samples/nextjs-app/deploy/build.sh
@@ -6,14 +6,10 @@ doit() {
   "$@"
 }
 
-ENVIRONMENT=nodejs-nextjs-env:latest
-doit docker build -t $ENVIRONMENT deploy/environment/ -f deploy/environment/Dockerfile-12.16
-doit kind load docker-image $ENVIRONMENT
-
-# doit docker push $ENVIRONMENT
-
 doit npm install
 doit npm run build
-doit zip -r nextjs-source.zip app.js next.config.js package.json package.lock.json data/ pages/ public/ styles/ node_modules/ yarn.lock .next/
+doit zip -r nextjs-source.zip app.js next.config.js package.json \
+  package.lock.json data/ pages/ public/ \
+  styles/ node_modules/ yarn.lock .next/
 
 doit fission spec apply --specdir='./deploy/specs'
--- samples/nextjs-app/deploy/specs/README.md
+++ samples/nextjs-app/deploy/specs/README.md
@@ -4,7 +4,7 @@
 
 ```sh
 fission spec init
-fission env create --spec --name nodejs --image fission/node-env:1.31.1
+fission env create --spec --name nodejs --image fission/node-env-12.16:1.31.1
 fission package create --spec --deploy nextjs-source.zip --env nodejs --name nextjs-source
 fission fn create --spec --name nextjs-func --pkgname nextjs-source --entrypoint "app"
 fission route create --spec --method GET --method POST --prefix "/nextapp/" --function nextjs-func --keeprefix
