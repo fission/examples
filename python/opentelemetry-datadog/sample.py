72cfa50b8976bfeeec4fe978c6654984ad032a06
techmaharaj <sharma.rockstar@gmail.com>
2022-01-06 13:59:48 +0530

    Reorganized the code

--- python/opentelemetry-datadog/sample.py
+++ python/opentelemetry-datadog/sample.py
@@ -6,27 +6,26 @@ from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+# Creating a reerouce with service.name attribute
+resource = Resource(attributes={
+    "service.name": "fission-sample-service"
+})
 
-def main():
+# Setting service name to the trace object
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
 
-    # Creating a reerouce with service.name attribute
-    resource = Resource(attributes={
-        "service.name": "fission-sample-service"
-    })
+# Initializing the OTLPExporter with the endpoint details
+otlp_exporter = OTLPSpanExporter(endpoint="otel-collector.opentelemetry-operator-system.svc:4317", insecure=True)
 
-    # Setting service name to the trace object
-    trace.set_tracer_provider(TracerProvider(resource=resource))
-    tracer = trace.get_tracer(__name__)
+# Setting up the processors and adding it to trace    
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
 
-    # Initializing the OTLPExporter with the endpoint details
-    otlp_exporter = OTLPSpanExporter(endpoint="otel-collector.opentelemetry-operator-system.svc:4317", insecure=True)
-
-    # Setting up the processors and adding it to trace    
-    span_processor = BatchSpanProcessor(otlp_exporter)
-    trace.get_tracer_provider().add_span_processor(span_processor)
+def main(): 
 
     # Creating our custom trace
     with tracer.start_as_current_span("parent"):
         print("Hello from Fission!")
-    
+
     return "Done"
