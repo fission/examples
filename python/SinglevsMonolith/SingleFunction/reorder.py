2807a895a2f81058d9baea149d3ba236fcd7b6e3
techmaharaj <sharma.rockstar@gmail.com>
2022-02-10 09:35:32 +0530

    Added Spec and Readme

--- python/SinglevsMonolith/SingleFunction/reorder.py
+++ python/SinglevsMonolith/SingleFunction/reorder.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 def main():
     data = request.json
     reorderQuantity = data['reorderquant']
-    if reorderQuantity<10:
+    if int(reorderQuantity) < 10:
         # Returning a fictious date, in reality you can call another function that will actually place an order.
         deldate = datetime.now() + timedelta(days=5)
     else:
