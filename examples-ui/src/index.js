8456b57a9934492bf33fc6c5853b3cef6f7e82c4
Jaseel <12792882+Spikatrix@users.noreply.github.com>
2022-08-03 11:31:32 +0530

    Fission examples UI (#80)
    * Initial commit
    
    * Style updates
    
    * Added search, improved styles and layout
    
    * Add fission logo

---
+++ examples-ui/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ExamplesUI from "./examples";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ExamplesUI />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
